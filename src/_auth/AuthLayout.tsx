0ce084abb5b7aac1231bbd57dddbbfb8b234cf8e
Lucas Torres <lucasgtorres1@gmail.com>
2024-02-02 18:47:55 -0300

    add form

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -14,7 +14,9 @@ const AuthLayout = () => {
             <Outlet/>
           </section>  
 
-          <img src=""/>
+          <img src="/assets/images/side-img.svg"
+          alt='logo'
+          className='hidden xl:block h-screen w-1/2 object-cover bg-no-repeat'/>
         </>  
       )}
     </>
